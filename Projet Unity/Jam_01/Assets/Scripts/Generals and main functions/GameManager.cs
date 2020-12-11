9db535a78af35d8ce245e3000de2b18dc4f81c82
Allosius27 <yanis.queroix@orange.fr>
2020-12-11 10:48:48 +0100

    Mise a jour Projet Unity : 11/12/2020 : 10:48

---
+++ Projet Unity/Jam_01/Assets/Scripts/Generals and main functions/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public PlayerMove playerMove;
+
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Il y a plus d'une instance de EnemiesDetection dans la scène");
+            return;
+        }
+
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(playerMove.countTurn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
